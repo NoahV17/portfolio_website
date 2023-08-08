dc48beb078f5cd9633e995182169c4395a14097a
NoahV17 <nvario1@gmail.com>
2023-08-08 13:48:22 -0400

    Full Reset on website and added JS

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    let text = document.getElementById("myInput").value;
+    document.getElementById("demo").innerHTML = "You wrote: " + text;
+}
+
+window.onscroll = function() {scrolling()};
+
+function scrolling() {
+    let pos = window.scrollY;
+    const elem = document.getElementById("bubbles");
+    if (pos < 150) {
+        elem.style.width = pos + "px";
+        elem.style.height = pos + "px";
+    } 
+    console.log("workingww");
+}
+
