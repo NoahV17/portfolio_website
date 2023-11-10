9eb5105dcfd6d6545e5268722722a0de8147d281
NoahV17 <nvario1@gmail.com>
2023-11-10 13:06:17 -0500

    added letter encryption site

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function myFunction() {
-    let text = document.getElementById("myInput").value;
-    document.getElementById("demo").innerHTML = "You wrote: " + text;
-}
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
+    var pq = document.getElementById("pqinput").value;
+    var e = document.getElementById("einput").value;
+    var input = document.getElementById("letterinput").value;
+
+    var output = Math.pow(alphabet.indexOf(input),e)%pq;
+
+    document.getElementById("calculate").innerHTML = output;
+  }
