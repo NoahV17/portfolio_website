17238088e052dd8eabe43a494d19d863e2214439
NoahV17 <nvario1@gmail.com>
2023-11-10 11:50:50 -0500

    added icons

--- script.js
+++ script.js
@@ -2,16 +2,3 @@ function myFunction() {
     let text = document.getElementById("myInput").value;
     document.getElementById("demo").innerHTML = "You wrote: " + text;
 }
-
-window.onscroll = function() {scrolling()};
-
-function scrolling() {
-    let pos = window.scrollY;
-    const elem = document.getElementById("bubbles");
-    if (pos < 200) {
-        elem.style.width = pos + "px";
-        elem.style.height = pos + "px";
-    } 
-    console.log("workingww");
-}
-
