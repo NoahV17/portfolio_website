e98ccca87afa14de65677c5aed099f5f3e8a9c3d
NoahV17 <nvario1@gmail.com>
2023-12-05 17:00:03 -0500

    QOL Improvements

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+$('#c_code').on('change',function(){
+  $('#code').val($(this).val());
+});
+
 //Code Block 1. Used for Encryption App
 
 function myFunction() {
@@ -12,8 +16,51 @@ function myFunction() {
       output += Math.pow(alphabet.indexOf(element),e)%pq + " ";
     }
 
-    document.getElementById("calculate").innerHTML = output;
+    document.getElementById("encrypted_output").innerHTML = output;
   }
 
 
 //Code block 2. Used for Currency Exchange App
+
+var https = require('https');
+
+function convertCurrency(amount, fromCurrency, toCurrency, cb) {
+  var apiKey = 'your-api-key-here';
+
+  fromCurrency = encodeURIComponent(fromCurrency);
+  toCurrency = encodeURIComponent(toCurrency);
+  var query = fromCurrency + '_' + toCurrency;
+
+  var url = 'https://free.currconv.com/api/v7/convert?q='
+            + query + '&compact=ultra&apiKey=' + apiKey;
+
+  https.get(url, function(res){
+      var body = '';  
+
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+
+      res.on('end', function(){
+          try {
+            var jsonObj = JSON.parse(body);
+
+            var val = jsonObj[query];
+            if (val) {
+              var total = val * amount;
+              cb(null, Math.round(total * 100) / 100);
+            } else {
+              var err = new Error("Value not found for " + query);
+              console.log(err);
+              cb(err);
+            }
+          } catch(e) {
+            console.log("Parse error: ", e);
+            cb(e);
+          }
+      });
+  }).on('error', function(e){
+        console.log("Got an error: ", e);
+        cb(e);
+  });
+}
