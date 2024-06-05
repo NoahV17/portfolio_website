c4f24657f112e986085757a730464f5dd85671e1
NoahV17 <nvario1@gmail.com>
2024-06-05 10:37:16 -0400

    Major change to oclor any layout of sites

--- script.js
+++ script.js
@@ -1,66 +1,22 @@
-$('#c_code').on('change',function(){
-  $('#code').val($(this).val());
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+// Toggle dropdown function
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
 });
 
-//Code Block 1. Used for Encryption App
-
-function myFunction() {
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"];
-    var pq = document.getElementById("pqinput").value;
-    var e = document.getElementById("einput").value;
-    var input = document.getElementById("letterinput").value;
-    var output = "";
-
-    for (let index = 0; index < input.length; index++) {
-      const element = input.substr(index, 1);
-      output += Math.pow(alphabet.indexOf(element),e)%pq + " ";
-    }
-
-    document.getElementById("encrypted_output").innerHTML = output;
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
   }
-
-
-//Code block 2. Used for Currency Exchange App
-
-var https = require('https');
-
-function convertCurrency(amount, fromCurrency, toCurrency, cb) {
-  var apiKey = 'your-api-key-here';
-
-  fromCurrency = encodeURIComponent(fromCurrency);
-  toCurrency = encodeURIComponent(toCurrency);
-  var query = fromCurrency + '_' + toCurrency;
-
-  var url = 'https://free.currconv.com/api/v7/convert?q='
-            + query + '&compact=ultra&apiKey=' + apiKey;
-
-  https.get(url, function(res){
-      var body = '';  
-
-      res.on('data', function(chunk){
-          body += chunk;
-      });
-
-      res.on('end', function(){
-          try {
-            var jsonObj = JSON.parse(body);
-
-            var val = jsonObj[query];
-            if (val) {
-              var total = val * amount;
-              cb(null, Math.round(total * 100) / 100);
-            } else {
-              var err = new Error("Value not found for " + query);
-              console.log(err);
-              cb(err);
-            }
-          } catch(e) {
-            console.log("Parse error: ", e);
-            cb(e);
-          }
-      });
-  }).on('error', function(e){
-        console.log("Got an error: ", e);
-        cb(e);
-  });
-}
+});
